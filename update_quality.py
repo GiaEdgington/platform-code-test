036c730011e2130502b9c98afdb3db2b59451a3a
giaedgington <gia.edgington@gmail.com>
2020-11-17 23:16:24 -0500

    Add Blue Star

--- update_quality.py
+++ update_quality.py
@@ -1,9 +1,13 @@
 def update_quality(awards):
     for award in awards:
+        award_loss = 1
+        if award.name == 'Blue Star':
+            award_loss = 2
+
         if award.name != 'Blue First' and award.name != 'Blue Compare':
             if award.quality > 0:
                 if award.name != 'Blue Distinction Plus':
-                    award.quality -= 1
+                    award.quality -= award_loss #Normal decrease
         else:
             if award.quality < 50:
                 award.quality += 1
@@ -23,7 +27,7 @@ def update_quality(awards):
                 if award.name != 'Blue Compare':
                     if award.quality > 0:
                         if award.name != 'Blue Distinction Plus':
-                            award.quality -= 1
+                            award.quality -= award_loss #Normal decrease if expired
                 else:
                     award.quality = award.quality - award.quality
             else:
